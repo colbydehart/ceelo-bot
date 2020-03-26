092796e329304b44afe0089de6e523f3c15c8868
Colby DeHart <colbydehart@gmail.com>
2020-03-25 20:29:38 -0500

    working with emoji reacts to register players

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
+// eslint-disable-next-line
 require('dotenv').config();
-const Bolt = require('@slack/bolt');
+const { App } = require('@slack/bolt');
+const Redis = require('ioredis');
 
-const slack = new Bolt.App({
+const redis = new Redis(process.env.REDIS_URL);
+
+const slack = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-slack.message('idiot', ({ say }) => {
+slack.message('idiot', async ({ say }) => {
   say('I AM IDIOT');
 });
 
-// slack.event('reaction_added', (/*{ event, context }*/) => {
-// TODO handle reactions to the initial message
-// });
+slack.message('play', async ({ message, client, context }) => {
+  const playing = await redis.get('playing');
+  if (parseInt(playing) !== 1) {
+    await redis.set('playing', 1);
+    const { ok, ts } = await client.chat.postMessage({
+      token: context.botToken,
+      channel: message.channel,
+      text: 'LETS PLAY CEELO. U REACT TO THIS WITH EMOJI TO PLAY',
+    });
+    if (ok) {
+      await redis.del('players');
+      await redis.sadd('players', [message.user]);
+      await redis.set('letsPlayMessage', ts);
+      await redis.set('currentPlayer', message.user);
+    }
+  }
+});
 
+slack.event('reaction_added', async ({ event }) => {
+  const { user, item } = event;
+  const letsPlayMessage = await redis.get('letsPlayMessage');
+  if (item.ts === letsPlayMessage) {
+    await redis.sadd('players', user);
+  }
+});
+
+slack.event('reaction_removed', async ({ event }) => {
+  const { user, item } = event;
+  const letsPlayMessage = await redis.get('letsPlayMessage');
+  if (item.ts === letsPlayMessage) {
+    await redis.srem('players', user);
+  }
+});
+
+// Restart the playing of redis
+redis.set('playing', 0);
+// Start the app
 console.log('🚀 STARTING...');
 slack.start(process.env.PORT || 3000);
