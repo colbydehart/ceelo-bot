3ee5dad9b7e80191b9e8f25fc2990a0543c5159b
Colby DeHart <colbydehart@gmail.com>
2020-12-07 21:13:26 -0600

    log config

--- config/config.exs
+++ config/config.exs
@@ -4,3 +4,9 @@ config :ceelo,
   slack_token: System.fetch_env!("SLACK_TOKEN"),
   slack_secret: System.fetch_env!("SLACK_SECRET") |> String.replace("\"", ""),
   port: String.to_integer(System.get_env("PORT", "3000"))
+
+config :logger,
+  compile_time_purge_matching: [
+    [application: :cowboy],
+    [level_lower_than: :info]
+  ]
