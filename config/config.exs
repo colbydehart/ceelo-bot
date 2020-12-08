953802da36a14e74b86129c7cdef1055937116f4
Colby DeHart <colbydehart@gmail.com>
2020-12-07 19:55:06 -0600

    woops port

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,4 @@ import Config
 config :ceelo,
   slack_token: System.fetch_env!("SLACK_TOKEN"),
   slack_secret: System.fetch_env!("SLACK_SECRET"),
-  port: 3000
+  port: String.to_integer(System.get_env("PORT", "3000"))
